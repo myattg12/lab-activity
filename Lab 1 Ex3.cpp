7f947afa48d434b4e41c74021bbf0cc526ade881
myattg12 <160702697+myattg12@users.noreply.github.com>
2024-02-21 01:34:46 -0500

    Add files via upload

---
+++ Lab 1 Ex3.cpp
@@ -0,0 +1,37 @@
+//
+//  main.cpp
+//  Lab 1 Homework 3
+//
+//  Created by myattg on 2/14/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // declaring variables
+    double num1, num2, num3, num4;
+    
+    // getting to know the first number
+    cout << "Enter the first number: ";
+    cin >> num1;
+    
+    // redo it again for 2nd one
+    cout << "Enter the second number:";
+    cin >> num2;
+    
+    //3rd number
+    cout << "Enter the third number:";
+    cin >> num3;
+    
+    //4th number
+    cout << "Enter the fourth number:" ;
+    cin >> num4;
+    
+    double average = (num1 + num2 + num3 + num4)/4;
+    
+    //declare the average definition
+    cout << "The average of these numbers is: " << average << endl;
+   
+    return 0;
+}
