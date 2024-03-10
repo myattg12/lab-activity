aced96d0c5007a0952c54e56e145adf45dc812e0
myattg12 <160702697+myattg12@users.noreply.github.com>
2024-03-09 20:33:29 -0500

    Add files via upload

---
+++ Lab1exercise1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string menu = 
+        "************************************************************\n"
+        "                        Welcome!\n"
+        "\n"
+        "Please choose a number from the following options:\n"
+        "\n"
+        "1.   Play the game!\n"
+        "2.   Demo the game!\n"
+        "3.   Exit\n"
+        "\n"
+        "************************************************************\n";
+    
+    cout << menu;
+    return 0;
+}
